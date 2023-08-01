7eae4b4d8caa179e9b2a6732002736db09a86c1f
Ali Seidu <aliseidu8855@gmail.com>
2023-08-01 15:27:54 +0000

    add

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function addTask() {
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
     let span = document.createElement("span");
-    span.innerHTML = "./u00d7";
+    span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
   inputBox.value = "";
